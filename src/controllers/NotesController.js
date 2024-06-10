af4646049a1368bb46b131abb7626105945170a5
Matheus Raphael <matheusraphael0902@gmail.com>
2024-06-10 16:55:53 -0300

    added function to show all notes

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -36,6 +36,64 @@ class NotesController {
       tags
     });
   }
+
+  async showAllNotes(request, response) {
+    const { user_id, title, tags } = request.query;
+
+    let notes;
+    let notesWithoutDuplicates;
+    let titleMap = {};
+
+    if(tags) {
+      const filterTags = tags.split(',').map(tag => tag.trim());
+      
+      notes = await knex("movieTags")
+      .select([
+        "movieNotes.id",
+        "movieNotes.title",
+        "movieNotes.rating",
+        "movieNotes.user_id",
+      ])
+      .where("movieNotes.user_id", user_id)
+      .whereLike("movieNotes.title", `%${title}%`)
+      .whereIn("name", filterTags)
+      .innerJoin("movieNotes", "movieNotes.id", "movieTags.note_id")
+      .orderBy("movieNotes.title");
+
+      notesWithoutDuplicates = notes.filter((obj) => {
+        if(!titleMap[obj.title]) {
+          titleMap[obj.title] = true;
+          return true
+        }
+        return false;
+      });
+
+      console.log(notesWithoutDuplicates);
+    }
+    else {
+      notes = await knex("movieNotes").where({ user_id }).whereLike("title", `%${title}%`).orderBy("title");
+    }
+
+    const userTags = await knex("movieTags").where({ user_id });
+    const notesWithTags = notesWithoutDuplicates.map(note => {
+      const noteTags = userTags.filter(tag => tag.note_id === note.id);
+
+      return {
+        ...note,
+        tags: noteTags
+      }
+    });
+
+    return response.json(notesWithTags);
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    await knex("movieNotes").where({ id }).delete();
+
+    return response.json();
+  }
 }
 
 module.exports = NotesController;
--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -8,5 +8,7 @@ const notesController = new NotesController();
 
 notesRoutes.post("/:user_id", notesController.create);
 notesRoutes.get("/:id", notesController.showNote);
+notesRoutes.get("/", notesController.showAllNotes);
+notesRoutes.delete("/:id", notesController.delete);
 
 module.exports = notesRoutes;
